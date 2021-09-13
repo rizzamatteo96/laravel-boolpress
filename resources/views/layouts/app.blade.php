bda8a03c70d5f3e68bc7efd2a7fc2bf484d45d75
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-13 15:59:28 +0200

    add dashboard to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    {{-- Fontawesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -72,9 +75,51 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="container-fluid">
+            <div class="row">
+                @auth
+                <nav class="col-2 sidebar py-5">
+                    <div class="sidebar-sticky">
+                        <ul class="nav flex-column border border-secondary rounded rounded-1">
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="{{ route('admin.index') }}">
+                                    <i class="fas fa-home"></i>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="#">
+                                    <i class="far fa-clipboard"></i>
+                                    Posts
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="#">
+                                    <i class="fas fa-users"></i>
+                                    Users
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="#">
+                                    <i class="fas fa-stream"></i>
+                                    Categories
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="#">
+                                    <i class="fas fa-tags"></i>
+                                    Tags
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                @endauth
+    
+                <main role="main" class="col-10 py-4">
+                    @yield('content')
+                </main>
+            </div>
     </div>
 </body>
 </html>
