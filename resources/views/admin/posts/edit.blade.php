c19bb87d5deeeea9b8ca50045082f0d82a0018cd
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-13 17:10:15 +0200

    change some grafic details

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -16,6 +16,15 @@
       <label for="description" class="form-label">Descrizione</label>
       <textarea type="password" class="form-control" id="description" name="description" rows="5">{{$post->description}}</textarea>
     </div>
-    <button type="submit" class="btn btn-primary">Salva le modifiche</button>
+    <div class="d-flex justify-content-between">
+      <a href="{{route('admin.posts.index')}}" class="btn btn-secondary">Torna indietro</a>
+      <button type="submit" class="btn btn-primary">Salva le modifiche</button>
+      <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST" class="d-inline-block">
+        {{-- Per ogni form bisogna inserire il token altrimenti il cambiamento non viene accettato dal sistema --}}
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger" onclick="return confirm('Sei sicuro di voler cancellare l\'elemento?')">Delete</button>
+      </form>
+    </div>
   </form>
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,7 +7,16 @@
   </div>
   <div class="card-body">
     <p class="card-text">{{$post->description}}</p>
-    <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna indietro</a>
   </div>
+</div>
+<div class="d-flex justify-content-between my-4">
+  <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna indietro</a>
+  <a href="{{route('admin.posts.edit', $post->id)}}" class="btn btn-warning">Edit</a>
+  <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST" class="d-inline-block">
+    {{-- Per ogni form bisogna inserire il token altrimenti il cambiamento non viene accettato dal sistema --}}
+    @csrf
+    @method('DELETE')
+    <button type="submit" class="btn btn-danger" onclick="return confirm('Sei sicuro di voler cancellare l\'elemento?')">Delete</button>
+  </form>
 </div>
 @endsection
